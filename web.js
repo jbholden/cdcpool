06e7d6c76c193e8ad5bb5383f767ab5600e7399a
jbholden <brent.l.holden@gmail.com>
2013-09-09 01:56:51 +0000

    code for connecting to postgresql

--- web.js
+++ web.js
@@ -2,9 +2,39 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var Sequelize = require("sequelize");
+
+//var sequelize = new Sequelize('postgres://ubuntu@localhost:5432/mydb'
+var sequelize = new Sequelize('mydb','postgres', 'midnight', {
+	host: '127.0.0.1',
+	port: 5432,
+	dialect: 'postgres'
 });
+
+sequelize.query("SELECT * FROM Games").success(function(myTableRows) {
+	console.log(myTableRows)
+});
+
+
+app.get('/', function(request, response) {
+  response.send('Overall Leaderboard');
+});
+
+app.param('wknum');
+
+app.get('/week/:wknum', function(request, response) {
+  response.send('Week ' + request.params.wknum + ' Leaderboard');
+});
+
+/*
+app.get('/week/leaderboard', function(request, response) {
+  response.send('Weekly Leaderboard');
+});
+
+app.get('/week/player/picks', function(request, response) {
+  response.send('Player picks');
+});
+*/
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
