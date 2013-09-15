369f27716a270d127e45a6ac29e3b1b4a66244c1
jbholden <brent.l.holden@gmail.com>
2013-09-15 01:24:34 +0000

    added jade and less

--- web.js
+++ web.js
@@ -1,20 +1,41 @@
 var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var Sequelize = require("sequelize");
+
 var app = express();
 app.use(express.logger());
 
-var Sequelize = require("sequelize");
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
 
 //var sequelize = new Sequelize('postgres://ubuntu@localhost:5432/mydb'
-var sequelize = new Sequelize('mydb','postgres', 'midnight', {
+var sequelize = new Sequelize('pooldb','postgres', 'football', {
 	host: '127.0.0.1',
 	port: 5432,
 	dialect: 'postgres'
 });
 
-sequelize.query("SELECT * FROM Games").success(function(myTableRows) {
+sequelize.query("SELECT * FROM weeks").success(function(myTableRows) {
 	console.log(myTableRows)
 });
 
+app.get('/routes', routes.index);
+app.get('/users', user.list);
 
 app.get('/', function(request, response) {
   response.send('Overall Leaderboard');
