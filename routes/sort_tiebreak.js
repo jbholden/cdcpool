cf133c8f63770dedca05cedeb38b4784c3267f2b
jbholden <brent.l.holden@gmail.com>
2013-11-08 03:15:17 +0000

    fixed tiebreaker page sort bug

--- routes/sort_tiebreak.js
+++ routes/sort_tiebreak.js
@@ -9,19 +9,27 @@ function sort_by_tiebreak(a,b) {
       if (a.number_of_tiebreaks == 4) {
          if (a.tiebreak3 == b.tiebreak3) { return 0; }
          if (a.tiebreak3 == "won") { return -1; }
+         if (a.tiebreak3 == "ahead") { return -1; }
          if (a.tiebreak3 == "lost") { return 1; }
+         if (a.tiebreak3 == "behind") { return 1; }
       } else if (a.number_of_tiebreaks == 3) {
          if (a.tiebreak2 == b.tiebreak2) { return 0; }
          if (a.tiebreak2 == "won") { return -1; }
+         if (a.tiebreak2 == "ahead") { return -1; }
          if (a.tiebreak2 == "lost") { return 1; }
+         if (a.tiebreak2 == "behind") { return 1; }
       } else if (a.number_of_tiebreaks == 2) {
          if (a.tiebreak1 == b.tiebreak1) { return 0; }
          if (a.tiebreak1 == "won") { return -1; }
+         if (a.tiebreak1 == "ahead") { return -1; }
          if (a.tiebreak1 == "lost") { return 1; }
+         if (a.tiebreak1 == "behind") { return 1; }
       } else if (a.number_of_tiebreaks == 1) {
          if (a.tiebreak0 == b.tiebreak0) { return 0; }
          if (a.tiebreak0 == "won") { return -1; }
+         if (a.tiebreak0 == "ahead") { return -1; }
          if (a.tiebreak0 == "lost") { return 1; }
+         if (a.tiebreak0 == "behind") { return 1; }
       } else if (a.number_of_tiebreaks == 0) {
          return 0;
       } else {
